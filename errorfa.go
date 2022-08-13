d2d5097dedb2ec56a74e44b95eb373fed229730a
lavsnow <amirmousavi0003@gmail.com>
2022-08-13 15:47:16 +0430

    add AppName

--- errorfa.go
+++ errorfa.go
@@ -6,19 +6,25 @@ type (
 	ErrorFarsi interface {
 		Error() string
 		ErrorFarsi() string
+		AppName() string
 		JSON() string
 	}
 	MyError struct {
+		Appname   string `json:"appName"`
 		Text      string `json:"error"`
 		TextFarsi string `json:"errorFarsi"`
 	}
 )
 
-func New(text, farsiText string) ErrorFarsi {
+func New(appName, text, farsiText string) ErrorFarsi {
 	return &MyError{
+		Appname:   appName,
 		Text:      text,
 		TextFarsi: farsiText,
 	}
+}
+func (e *MyError) AppName() string {
+	return e.Appname
 }
 
 func (e *MyError) Error() string {
